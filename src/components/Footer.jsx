86f95d2d161d792fc87cd99dbcb2272eecec452e
Aman Vishwakarma <123929292+superAman07@users.noreply.github.com>
2024-11-09 19:49:41 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,6 @@ export default function Footer() {
             <p className="text-lg font-medium">
                 Made by <span className="text-amber-400">Aman Vishwakarma</span>
             </p>
-            <div className="mt-2">
-                <h1 className="text-xl font-semibold text-amber-400">Technologies Used</h1>
-                <p className="text-md">React and Tailwind CSS for Front-End</p>
-                <p className="text-md">Express for Back-End</p>
-            </div>
             <div className="mt-2 text-gray-500 text-sm">
                 © {new Date().getFullYear()} Aman Vishwakarma. All rights reserved.
             </div>
